71f956c041871ce094414348e581bdb2ec4c23a0
jesus-dev7 <kids.anguita@gmail.com>
2025-12-05 10:25:59 +0100

    a

--- Referee/main.py
+++ Referee/main.py
@@ -31,6 +31,7 @@ while True:
             incoming = None
     if PlayerNum == 2:
         for i in range(3):
+            TwoReps = 0
             while True:
                 if button_b.is_pressed():
                     radio.send(str("Fight !"))
@@ -52,18 +53,23 @@ while True:
                                 Res1 = incoming[:5]
                                 display.scroll(incoming)
                                 display.scroll(Res1)
+                                RefM1 = dico[incoming[-4:]]
                                 dico[incoming[-4:]].ToStack(Res1)
-                                display.scroll(str(Memory1._stack))
+                                display.scroll(str(RefM1._stack))
 
                             elif incoming[-4:] == Ping2:
                                 TwoReps += 1
                                 Res2 = incoming[:5]
                                 display.scroll(incoming)
                                 display.scroll(Res2)
+                                RefM2 = dico[incoming[-4:]]
                                 dico[incoming[-4:]].ToStack(Res2)
-                                display.scroll(str(Memory2._stack))
+                                display.scroll(str(RefM2._stack))
 
                             display.scroll(TwoReps)
+                            if button_a.is_pressed():
+                                if button_b.is_pressed():
+                                    TwoReps += 1
                             if TwoReps >1:
                                 while True:
                                     if button_a.is_pressed():
@@ -73,15 +79,33 @@ while True:
                                         Winn = CheckWinner(Res1, Res2)
                                         display.scroll(str(Winn))
                                         if Winn == "1: Won !":
-                                            Memory1.ToUnStack()
-                                            Memory1.ToStack(0)
-                                            Memory2.ToUnStack()
-                                            Memory2.ToStack(1)
+                                            RefM1.ToUnStack()
+                                            RefM1.ToStack(0)
+                                            RefM2.ToUnStack()
+                                            RefM2.ToStack(1)
                                         elif Winn == "2: Won !":
-                                            Memory2.ToUnStack()
-                                            Memory2.ToStack(0)
-                                            Memory1.ToUnStack()
-                                            Memory1.ToStack(1)
-                                        display.scroll(str(Memory1._stack))
-                                        display.scroll(str(Memory2._stack))
+                                            RefM2.ToUnStack()
+                                            RefM2.ToStack(0)
+                                            RefM1.ToUnStack()
+                                            RefM1.ToStack(1)
+                                        elif Winn == "T: Tie !":
+                                            RefM2.ToUnStack()
+                                            RefM2.ToStack(0)
+                                            RefM1.ToUnStack()
+                                            RefM1.ToStack(0)
+                                        display.scroll(str(RefM1._stack))
+                                        display.scroll(str(RefM2._stack))
                                         break
+                                break
+                                J1P = 0
+                                J2P = 0
+                                for points in RefM1._stack:
+                                    J1P += points
+                                for points in RefM2._stack:
+                                    J2P += points
+                                display.scroll(J1P)
+                                display.scroll(J2P)
+                                if J1P > J2P:
+                                    display.scroll("J1 wins")
+                                else:
+                                    display.scroll("J2 wins")
